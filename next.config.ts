c9d8d26f61287a7f4b5f121d8a3af9b27f54a981
Pablo Romano <pabloezeromano@gmail.com>
2026-05-26 09:20:55 -0300

    chore: update base path
    Signed-off-by: Pablo Romano <pabloezeromano@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+// next.config.mjs
+// const isDev = process.env.NODE_ENV !== 'production';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/field-crm',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/field-crm',
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
